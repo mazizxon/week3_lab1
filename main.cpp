73e07d409aa4ca6bae47ab997ebc79c362f5bb3f
m.azizxonov <m.azizxonov@newuu.uz>
2025-09-09 11:21:18 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Please enter two integers: ";
+    cin >> a >> b;
+    cout << "Sum: " << a << " + " << b << " = " << a + b << endl;
+    return 0;
+
+}
+
